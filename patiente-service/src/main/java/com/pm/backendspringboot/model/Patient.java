89310958ca09cf2b4a7ea9bc722ed033fe7d62a7
Luiz Messias <luizgdsmdev@gmail.com>
2026-07-03 06:41:39 -0300

    Add(data): add basic data for patient listing test

---
+++ patiente-service/src/main/java/com/pm/backendspringboot/model/Patient.java
@@ -0,0 +1,120 @@
+package com.pm.backendspringboot.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    @Size(max = 30, min = 1)
+    private String firstName;
+
+    @NotNull
+    @Size(max = 30, min = 1)
+    private String lastName;
+
+    @NotNull
+    @Email
+    @Size(max = 60, min = 1)
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    @Size(max = 120, min = 1)
+    private String address;
+
+    @NotNull
+    private LocalDate birthDate;
+
+    @NotNull
+    private Boolean active;
+
+    @NotNull
+    @CreationTimestamp
+    private LocalDate registrationDate;
+
+    @NotNull
+    @UpdateTimestamp
+    private LocalDate lastUpdate;
+
+    public LocalDate getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDate lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
